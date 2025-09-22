786df70d991ea545a3201bf103d22671fb301897
Louay Daher <louaydaher173@gmail.com>
2025-09-22 17:26:10 +0300

    product crud

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { response } from "express"
 import mongoose from "mongoose"
 import userRoute from "./routes/userRoute.js"
-
+import { seedInitialProducts } from "./services/productService.js";
+import productRoute from "./routes/productRoute.js"
 const app=express();
 const port=3000;
 
@@ -14,11 +15,12 @@ mongoose.connect('mongodb://localhost:27017/Ecommerce2').then(
 ).catch((err)=>{
     console.log("Error connecting to mongodb",err)
 });
-app.use('/user',userRoute)
+app.use('/user',userRoute);
+app.use('/product',productRoute);
 app.get('/', (request, response) => {
     response.send('Hello Worrld!');
 });
-
+seedInitialProducts();
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
---
+++ src/services/productService.ts
@@ -0,0 +1,27 @@
+import productModel from "../models/productModel.js";
+  
+export const getAllProducts=async ()=>{
+    return await productModel.find();
+}
+export const seedInitialProducts=async ()=>{
+    const products = [
+        {
+          title: "Wireless Headphones",
+          image: "https://picsum.photos/seed/headphones/400/300",
+          price: 120,
+          stock: 80,
+        },
+        {
+          title: "Smart Watch",
+          image: "https://picsum.photos/seed/watch/400/300",
+          price: 150,
+          stock: 95,
+        },
+        
+      ];
+       const existingProducts=await getAllProducts();
+       if (existingProducts.length===0){
+        await productModel.insertMany(products);
+       }
+}
+
