b1a34c5c4bada4e3ce39bb3c1f51a7223275f472
Louay Daher <louaydaher173@gmail.com>
2025-10-01 19:19:29 +0300

    MUI Installation +Nabvar

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
   <BrowserRouter>
-  <Routes>
+  <Navbar/> 
+  <Routes> 
     <Route path='/' element={<HomePage/>}/>
   </Routes>
   </BrowserRouter>
